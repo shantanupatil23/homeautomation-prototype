1039c6f5ecd95a77a2c6bea46cbbbb4357573602
shantanu354 <kanhapatildurg@gmail.com>
2020-05-06 02:56:13 +0530

    v2_Front-End Final

---
+++ app/src/main/java/com/example/homeautomation/splash_screen.java
@@ -0,0 +1,27 @@
+package com.example.homeautomation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash_screen extends AppCompatActivity {
+    private static int SPLASH = 355;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
+    }
+}
