e43beceeb23761cefab24056c7087bfb504491e1
shantanu354 <kanhapatildurg@gmail.com>
2020-05-18 22:19:02 +0530

    v2_Switch Config popup added

--- app/src/main/java/com/example/homeautomation/MainActivity.java
+++ app/src/main/java/com/example/homeautomation/MainActivity.java
@@ -48,17 +48,17 @@ public class MainActivity extends AppCompatActivity {
     int loading = 0;
     Boolean isAutoON = false;
     View auto_bar;
-    PopupWindow popupWindow;
-    LinearLayout linearLayout1;
-    DatabaseReference phone_auto_firebase, phone_switch_firebase, laptop_switch_firebase, extra_switch_firebase, auto_start, auto_end;
-    EditText editText_auto_start, editText_auto_end;
-    View popupView;
+    DatabaseReference phone_auto_firebase, phone_switch_firebase, laptop_switch_firebase, extra_switch_firebase, auto_start, auto_end, switch_phone_firebase, switch_laptop_firebase, switch_extra_firebase;
+    EditText editText_auto_start, editText_auto_end, editText_socket_phone, editText_socket_laptop,editText_socket_extra;
+    View popupView_auto, popupView_sockets;
+    PopupWindow popupWindow_auto, popupWindow_sockets;
+    LinearLayout layout_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        linearLayout1 = findViewById(R.id.activity_home);
+        layout_main = findViewById(R.id.activity_home);
 
         Toolbar my_toolbar = findViewById(R.id.action_bar);
         my_toolbar.setTitle("");
@@ -245,17 +245,116 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id){
-            case R.id.config_switches:
+            case R.id.config_sockets:
+                LayoutInflater config_sockets_inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                popupView_sockets = config_sockets_inflater.inflate(R.layout.popup_switch_config,null);
+                popupWindow_sockets = new PopupWindow(popupView_sockets, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
+                popupWindow_sockets.setElevation(100);
+                popupWindow_sockets.showAtLocation(layout_main, Gravity.CENTER, 0, 0);
+
+                editText_socket_phone = popupView_sockets.findViewById(R.id.edittext_phone_socket);
+                switch_phone_firebase = FirebaseDatabase.getInstance().getReference().child("NodeMCU").child("switch_id_phone");
+                switch_phone_firebase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Integer value = dataSnapshot.getValue(Integer.class);
+                        assert value != null;
+                        editText_socket_phone.setHint(String.valueOf(value));
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
+                });
+
+                editText_socket_laptop = popupView_sockets.findViewById(R.id.edittext_laptop_socket);
+                switch_laptop_firebase = FirebaseDatabase.getInstance().getReference().child("NodeMCU").child("switch_id_laptop");
+                switch_laptop_firebase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Integer value = dataSnapshot.getValue(Integer.class);
+                        assert value != null;
+                        editText_socket_laptop.setHint(String.valueOf(value));
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
+                });
+
+                editText_socket_extra = popupView_sockets.findViewById(R.id.edittext_extra_socket);
+                switch_extra_firebase = FirebaseDatabase.getInstance().getReference().child("NodeMCU").child("switch_id_extra");
+                switch_extra_firebase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Integer value = dataSnapshot.getValue(Integer.class);
+                        assert value != null;
+                        editText_socket_extra.setHint(String.valueOf(value));
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
+                });
+
+
+                Button save_button_switch = popupView_sockets.findViewById(R.id.button_switch_save);
+                save_button_switch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String str_phone = editText_socket_phone.getText().toString(), str_laptop = editText_socket_laptop.getText().toString(), str_extra = editText_socket_extra.getText().toString();
+                        int switch_phone, switch_laptop, switch_extra;
+                        boolean isValid = true;
+                        if (!str_phone.equals("")){
+                            if (!str_phone.equals(str_laptop) && !str_phone.equals(str_extra)){
+                                switch_phone = Integer.parseInt(str_phone);
+                                switch_phone_firebase.setValue(switch_phone);
+                            }
+                            else{
+                                isValid = false;
+                                Toast.makeText(getApplicationContext(), "Two switches can not be same", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        if (!str_laptop.equals("")){
+                            if (!str_laptop.equals(str_phone) && !str_laptop.equals(str_extra)){
+                                switch_laptop = Integer.parseInt(str_laptop);
+                                switch_laptop_firebase.setValue(switch_laptop);
+                            }
+                            else{
+                                isValid = false;
+                                Toast.makeText(getApplicationContext(), "Two switches can not be same", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        if (!str_extra.equals("")){
+                            if (!str_extra.equals(str_phone) && !str_extra.equals(str_laptop)){
+                                switch_extra = Integer.parseInt(str_extra);
+                                switch_extra_firebase.setValue(switch_extra);
+                            }
+                            else{
+                                isValid = false;
+                                Toast.makeText(getApplicationContext(), "Two switches can not be same", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        if (isValid){
+                            popupWindow_sockets.dismiss();
+                            Toast.makeText(getApplicationContext(), "Changes Saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                Button cancel_button_switch = popupView_sockets.findViewById(R.id.button_switch_cancel);
+                cancel_button_switch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow_sockets.dismiss();
+                    }
+                });
+
                 break;
+
             case R.id.change_auto_details:
-                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                popupView = layoutInflater.inflate(R.layout.popup_autocharge_config,null);
-                popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
-                popupWindow.setElevation(100);
-                popupWindow.showAtLocation(linearLayout1, Gravity.CENTER, 0, 0);
+                LayoutInflater auto_config_inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                popupView_auto = auto_config_inflater.inflate(R.layout.popup_autocharge_config,null);
+                popupWindow_auto = new PopupWindow(popupView_auto, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
+                popupWindow_auto.setElevation(100);
+                popupWindow_auto.showAtLocation(layout_main, Gravity.CENTER, 0, 0);
 
                 auto_start = FirebaseDatabase.getInstance().getReference().child("NodeMCU").child("auto_phone_begin");
-                editText_auto_start = popupView.findViewById(R.id.edittext_auto_start);
+                editText_auto_start = popupView_auto.findViewById(R.id.edittext_auto_start);
                 auto_start.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -267,7 +366,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onCancelled(@NonNull DatabaseError error) {}
                 });
                 auto_end = FirebaseDatabase.getInstance().getReference().child("NodeMCU").child("auto_phone_end");
-                editText_auto_end = popupView.findViewById(R.id.edittext_auto_end);
+                editText_auto_end = popupView_auto.findViewById(R.id.edittext_auto_end);
                 auto_end.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -280,8 +379,8 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
-                Button save_button = popupView.findViewById(R.id.button_auto_save);
-                save_button.setOnClickListener(new View.OnClickListener() {
+                Button save_button_auto = popupView_auto.findViewById(R.id.button_auto_save);
+                save_button_auto.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         String str_start = editText_auto_start.getText().toString(), str_end = editText_auto_end.getText().toString();
@@ -294,16 +393,16 @@ public class MainActivity extends AppCompatActivity {
                             auto_end_int = Integer.parseInt(str_end);
                             auto_end.setValue(auto_end_int);
                         }
-                        popupWindow.dismiss();
+                        popupWindow_auto.dismiss();
                         Toast.makeText(getApplicationContext(), "Changes Saved", Toast.LENGTH_SHORT).show();
                     }
                 });
 
-                Button cancel_button = popupView.findViewById(R.id.button_auto_cancel);
-                cancel_button.setOnClickListener(new View.OnClickListener() {
+                Button cancel_button_auto = popupView_auto.findViewById(R.id.button_auto_cancel);
+                cancel_button_auto.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        popupWindow.dismiss();
+                        popupWindow_auto.dismiss();
                     }
                 });
                 break;
